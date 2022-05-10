2f17b54f12731aeba3f89725d6df1dab2d0e293a
Shreya Pawaskar <shreyap2550@gmail.com>
2022-05-10 17:42:34 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -17,13 +17,13 @@ EPOCH = 200
 BATCH_SIZE = 200
 WORK_DIR = "/content/SilencerEnhancerPredict"
 
-def model_predict(data_file, weights_file, result_file):
+def model_predict(data_file, weights_file, result_file,X_test_kf):
 
   model_file = WORK_DIR + "/examples/model.hdf5"
   model = load_model(model_file)
   model.load_weights(weights_file)
   data = load_dataset(data_file)
-  x = data["test_data"]
+  x = X_test_kf
 
   print("prediction on test samples ...")
   ypred = model.predict(x, batch_size=200, verbose=1)
